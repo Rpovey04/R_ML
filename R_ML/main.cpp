4959364fcdfe50a935a550cac861db3531e311b8
Rhys Povey <rhys.povey82@gmail.com>
2024-10-18 20:59:44 +0100

    lim simple configured for testing

--- R_ML/main.cpp
+++ R_ML/main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
+#include "limsimple/Window.h"
 
 int main() {
-	std::cout << "Hello world" << std::endl;
-	return 1;
+	std::string p1 = "../../../ExampleTexture/00000001_005.jpg";
+	std::string p2 = "../../../ExampleTexture/00000001_000.jpg";
+
+	int ticks = 0;
+	Window myWindow("LimSimple", 500, 375);
+	myWindow.displayImage(p1);
+	while (!myWindow.shouldClose()) {
+		if (ticks++ == 100000) {
+			std::cout << "switching" << std::endl;
+			myWindow.displayImage(p2);
+		}
+		myWindow.pollOnce();
+	}
 }
